2dd9bed50751dda0705b5b7ef2132c28320b24a4
Md. Imran Mir <imranmir6677@gmail.com>
2023-04-11 17:40:52 +0600

    c++ solution of Container With Most Water

--- C++ solutions/11. Container With Most Water.cpp
+++ C++ solutions/11. Container With Most Water.cpp
@@ -8,18 +8,12 @@ public:
         long long mx=0;
         int len=height.size();
         for(int i=0,j=len-1;i<j;){
-            if(height[i]<=height[j]){
-                long long ans=min(height[i],height[j]);
-                ans*=abs(i-j);
-                mx=max(mx,ans);
-                i++;
-            }
-            else{
-                long long ans=min(height[i],height[j]);
-                ans*=abs(i-j);
-                mx=max(mx,ans);
-                j--;
-            }
+            long long ans=min(height[i],height[j]);
+            ans*=abs(i-j);
+            mx=max(mx,ans);
+
+            if(height[i]<=height[j]) i++;
+            else j--;
         }
         return mx;
     }
