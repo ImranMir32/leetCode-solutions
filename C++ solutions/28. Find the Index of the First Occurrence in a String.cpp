1c9c15b3a8c91bd1bc6b1b9f2b9e177423a26cf0
Md. Imran Mir <imranmir6677@gmail.com>
2023-04-23 00:10:47 +0600

    adding c++ solution

---
+++ C++ solutions/28. Find the Index of the First Occurrence in a String.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// solution 
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        return haystack.find(needle);
+    }
+};
