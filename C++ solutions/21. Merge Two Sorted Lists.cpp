e7c109f8d16ed8fc7a556ed6e4fd7b774e09d7bd
Md. Imran Mir <imranmir6677@gmail.com>
2023-04-23 18:53:42 +0600

    adding c++ solution

---
+++ C++ solutions/21. Merge Two Sorted Lists.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// solution
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode head(0);
+        ListNode *tail=&head;
+        //head.next=tail;
+        while(list1 && list2){
+            if(list1->val < list2->val){
+                tail->next=list1;
+                list1=list1->next;
+            }
+            else{
+                tail->next=list2;
+                list2=list2->next;
+            }
+            tail=tail->next;
+        }
+    
+        list1 ? tail->next=list1 : tail->next=list2;
+        return head.next;
+    }
+};
