54ade3702ea28dc049b0e1718bf7fd6b19c85fcf
Md. Imran Mir <imranmir6677@gmail.com>
2023-04-26 00:06:45 +0600

    adding c++ solution

---
+++ C++ solutions/202. Happy Number.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// solution 
+class Solution
+{
+    public:
+        int next(int n)
+        {
+            int sum = 0;
+            while (n != 0)
+            {
+                sum += pow(n % 10, 2);
+                n = n / 10;
+            }
+            return sum;
+        }
+    public:
+        bool isHappy(int n)
+        {
+            int slow = next(n);
+            int fast = next(next(n));
+            while (slow != fast)
+            {
+                slow = next(slow);
+                fast = next(next(fast));
+            }
+            return fast == 1;
+        }
+};
