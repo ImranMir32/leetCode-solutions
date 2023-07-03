31d2c54f65c59190f61b72dc51980d0f95caa509
Md. Imran Mir <imranmir6677@gmail.com>
2023-07-04 00:46:19 +0600

    adding JS solution

---
+++ JavaScript solutions/12. Integer to Roman.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number} num
+ * @return {string}
+ */
+var intToRoman = function (num) {
+  const IntRoman = [
+    [1000, "M"],
+    [900, "CM"],
+    [500, "D"],
+    [400, "CD"],
+    [100, "C"],
+    [90, "XC"],
+    [50, "L"],
+    [40, "XL"],
+    [10, "X"],
+    [9, "IX"],
+    [5, "V"],
+    [4, "IV"],
+    [1, "I"],
+  ];
+
+  let Roman = "";
+
+  for (let i = 0; i < IntRoman.length; i++) {
+    while (num >= IntRoman[i][0]) {
+      Roman += IntRoman[i][1];
+      num -= IntRoman[i][0];
+    }
+  }
+
+  return Roman;
+};
