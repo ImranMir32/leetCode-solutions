e0990aba84a99cd6d18afb20444b208002a025f4
Md. Imran Mir <imranmir6677@gmail.com>
2023-08-08 01:57:01 +0600

    Adding Js solution

--- JavaScript solutions/852. Peak Index in a Mountain Array.js
+++ JavaScript solutions/852. Peak Index in a Mountain Array.js
@@ -6,7 +6,7 @@ var peakIndexInMountainArray = function (arr) {
   let left = 0,
     right = arr.length - 1;
   while (left <= right) {
-    let mid = (left + right) / 2;
+    let mid = Math.floor((left + right) / 2);
     if (arr[mid - 1] < arr[mid] && arr[mid] > arr[mid + 1]) return mid;
     else if (arr[mid - 1] < arr[mid] && arr[mid] < arr[mid + 1]) left = mid + 1;
     else right = mid;
