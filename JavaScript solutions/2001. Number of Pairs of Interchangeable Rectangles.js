beff57d74c335b5efabdcac8c8f5ed428901aaab
Md. Imran Mir <imranmir6677@gmail.com>
2023-08-11 23:13:44 +0600

    adding js solution

---
+++ JavaScript solutions/2001. Number of Pairs of Interchangeable Rectangles.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[][]} rectangles
+ * @return {number}
+ */
+var interchangeableRectangles = function (rectangles) {
+  let m = new Map();
+  for (let i = 0; i < rectangles.length; i++) {
+    let width = rectangles[i][0];
+    let height = rectangles[i][1];
+    let aspectRatio = width / height;
+
+    if (!m.has(aspectRatio)) {
+      m.set(aspectRatio, 1);
+    } else {
+      m.set(aspectRatio, m.get(aspectRatio) + 1);
+    }
+  }
+
+  let ans = 0;
+  m.forEach((value) => {
+    if (value > 1) {
+      let n = value - 1;
+      ans += (n * (n + 1)) / 2;
+    }
+  });
+
+  return ans;
+};
