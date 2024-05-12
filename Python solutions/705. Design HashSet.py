3c640a96735903dbf4e5fa76e805a0d39bc95180
Md. Imran Mir <imranmir6677@gmail.com>
2024-05-12 21:44:53 +0600

    Adding python solution

---
+++ Python solutions/705. Design HashSet.py
@@ -0,0 +1,19 @@
+class MyHashSet:
+    def __init__(self):
+        self.hash = [False] * 1000007
+
+    def add(self, key: int) -> None:
+        self.hash[key] = True
+
+    def remove(self, key: int) -> None:
+        self.hash[key] = False
+
+    def contains(self, key: int) -> bool:
+        return self.hash[key]
+
+
+# Your MyHashSet object will be instantiated and called as such:
+# obj = MyHashSet()
+# obj.add(key)
+# obj.remove(key)
+# param_3 = obj.contains(key)
